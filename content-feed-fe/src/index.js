a14aa0688e0cb32907bb1af0a271bda39ecf8d12
rajeshkumarkarra <rajeshkumarkarra@outlook.com>
2019-07-26 17:10:54 +0530

    little changes added in js

---
+++ content-feed-fe/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class ContentFeed extends React.Component {
+	componentDidMount() {
+		this.getItems();
+	}
+
+	getItems() {
+		fetch('https://127.0.0.1:8000/api/item/')
+			.then(results => results.json())
+			.then(results => console.log(results))
+	}
+	render() {
+		return null;
+	}
+}
+
+
+
+ReactDOM.render(
+	<ContentFeed />,
+	document.getElementById('root')
+
+);
